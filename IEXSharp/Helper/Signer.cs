983bc12cf3462372770f9bb9f0283ed4897e1298
vslee <dev-github@vslee.com>
2019-12-27 22:49:38 -0800

    changed Signer set() to properties
    - changed SetHost to Host
    - changed SetSecretToken to SecretToken

--- IEXSharp/Helper/Signer.cs
+++ IEXSharp/Helper/Signer.cs
@@ -10,16 +10,16 @@ namespace VSLee.IEXSharp.Helper
 	/// usage:
 	/// string host = "sandbox.iexapis.com"; //Sandbox host
 	/// //string host = "cloud.iexapis.com" //Production host
-	/// string pk = "PublishToken";
-	/// string sk = "SecretToken";
+	/// string publishableToken = "PublishableToken";
+	/// string secretToken = "SecretToken";
 	/// 
-	/// IEXSigner.Signer signer = new IEXSigner.Signer(host, sk);
+	/// IEXSigner.Signer signer = new IEXSigner.Signer(host, secretToken);
 	/// 
 	/// /*
 	/// * If run separately
 	/// IEXSigner.Signer signer = new IEXSigner.Signer();
-	/// signer.SetHost(host);
-	/// signer.SetSecretToken(sk);
+	/// signer.Host = host;
+	/// signer.SecretToken = secretToken;
 	/// */
 	/// 
 	/// string method = "GET";
@@ -28,46 +28,37 @@ namespace VSLee.IEXSharp.Helper
 	/// string payload = "{\"test\":123,\"obj\":456}";
 	/// 
 	/// //For GET requests
-	/// (string iexdate, string authorization_header) headers = signer.Sign(pk, method, url, queryString);
+	/// (string iexdate, string authorization_header) headers = signer.Sign(publishableToken, method, url, queryString);
 	/// 
 	/// //For POST requests
-	/// (string iexdate, string authorization_header) headers = signer.Sign(pk, method, url, queryString, payload);
+	/// (string iexdate, string authorization_header) headers = signer.Sign(publishableToken, method, url, queryString, payload);
 	/// 
 	/// </summary>
 	internal class Signer
 	{
-		string host;
-		string secretToken;
-
 		public Signer() { }
 
 		public Signer(string host, string secretToken) : this()
 		{
-			this.host = host;
-			this.secretToken = secretToken;
+			this.Host = host;
+			this.SecretToken = secretToken;
 		}
 
-		public void SetHost(string host)
-		{
-			this.host = host;
-		}
+		public string Host { private get; set; }
 
-		public void SetSecretToken(string secretToken)
-		{
-			this.secretToken = secretToken;
-		}
+		public string SecretToken { private get; set; }
 
 		public (string iexdate, string authorization_header) Sign(string publishableToken, string method, string url, string queryString, string payload = "")
 		{
 			var now = DateTime.UtcNow;
 			var iexdate = now.ToString("yyyyMMddTHHmmssZ");
 			var datestamp = now.ToString("yyyyMMdd");
-			var canonical_headers = $"host:{host}\nx-iex-date:{iexdate}\n";
+			var canonical_headers = $"host:{Host}\nx-iex-date:{iexdate}\n";
 			var payload_hash = SHA256HexHashString(payload);
 			var canonical_request = $"{method}\n{url}\n{queryString}\n{canonical_headers}\nhost;x-iex-date\n{payload_hash}";
 			var credential_scope = $"{datestamp}/iex_request";
 			var string_to_sign = $"IEX-HMAC-SHA256\n{iexdate}\n{credential_scope}\n{SHA256HexHashString(canonical_request)}";
-			var signing_key = HMACSHA256HexHashString(HMACSHA256HexHashString(secretToken, datestamp), "iex_request");
+			var signing_key = HMACSHA256HexHashString(HMACSHA256HexHashString(SecretToken, datestamp), "iex_request");
 			var signature = HMACSHA256HexHashString(signing_key, string_to_sign);
 			var authorization_header = $"IEX-HMAC-SHA256 Credential={publishableToken}/{credential_scope}, SignedHeaders=host;x-iex-date, Signature={signature}";
 			return (iexdate, authorization_header);
