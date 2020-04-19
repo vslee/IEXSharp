ea87b8ae55cba484ceb0873645c4e66aa3cc9b00
vslee <dev-github@vslee.com>
2020-04-19 16:04:28 -0700

    remove extraneous _pk from ForexCurrenciesService V2

--- IEXSharp/Service/V2/ForexCurrencies/ForexCurrenciesService.cs
+++ IEXSharp/Service/V2/ForexCurrencies/ForexCurrenciesService.cs
@@ -9,12 +9,10 @@ namespace VSLee.IEXSharp.Service.V2.ForexCurrencies
 {
 	internal class ForexCurrenciesService : IForexCurrenciesService
 	{
-		private readonly string _pk;
 		private readonly ExecutorREST _executor;
 
 		public ForexCurrenciesService(HttpClient client, string sk, string pk, bool sign)
 		{
-			_pk = pk;
 			_executor = new ExecutorREST(client, sk, pk, sign);
 		}
 
