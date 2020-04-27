39519fcf567fcb12d05ef4fc12fb64fb6588e7c9
JamiePrentice <2858046+JamiePrentice@users.noreply.github.com>
2020-04-27 21:20:12 +0100

    WIP Crypto Tests

--- IEXSharpTest/Cloud(V2)/CryptoTest.cs
+++ IEXSharpTest/Cloud(V2)/CryptoTest.cs
@@ -1,5 +1,4 @@
 using NUnit.Framework;
-using System;
 using System.Threading.Tasks;
 using VSLee.IEXSharp;
 
@@ -13,6 +12,26 @@ namespace VSLee.IEXSharpTest.Cloud
 		public void Setup()
 		{
 			sandBoxClient = new IEXCloudClient(TestGlobal.pk, TestGlobal.sk, false, true);
+		}
+
+		[Test]
+		[TestCase("BTCUSDT")]
+		public async Task BookAsyncTest(string symbol)
+		{
+			var response = await sandBoxClient.Crypto.BookAsync(symbol);
+
+			Assert.IsNull(response.ErrorMessage);
+			Assert.IsNotNull(response.Data);
+		}
+
+		[Test]
+		[TestCase("BTCUSDT")]
+		public async Task PriceAsyncTest(string symbol)
+		{
+			var response = await sandBoxClient.Crypto.PriceAsync(symbol);
+
+			Assert.IsNull(response.ErrorMessage);
+			Assert.IsNotNull(response.Data);
 		}
 
 		[Test]
