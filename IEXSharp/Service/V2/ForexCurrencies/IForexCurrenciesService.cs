c7efc82aab0d60766ffef893bcfe51a09ebc8dc6
JamiePrentice <2858046+JamiePrentice@users.noreply.github.com>
2020-04-28 22:20:38 +0100

    Updated code docs

--- IEXSharp/Service/V2/ForexCurrencies/IForexCurrenciesService.cs
+++ IEXSharp/Service/V2/ForexCurrencies/IForexCurrenciesService.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Data;
 using IEXSharp.Model;
 using System.Threading.Tasks;
 using VSLee.IEXSharp.Model.ForexCurrencies.Response;
@@ -18,21 +19,24 @@ namespace VSLee.IEXSharp.Service.V2.ForexCurrencies
 		/// <summary>
 		/// <see cref="https://iexcloud.io/docs/api/#latest-currency-rates"/>
 		/// </summary>
-		/// <value>700</value>
+		/// <param name="symbols"></param>
 		/// <returns></returns>
 		Task<IEXResponse<IEnumerable<CurrencyRateResponse>>> LatestRatesAsync(string symbols);
 
 		/// <summary>
 		/// <see cref="https://iexcloud.io/docs/api/#currency-conversion"/>
 		/// </summary>
-		/// <value>700</value>
+		/// <param name="symbols"></param>
+		/// <param name="amount"></param>
 		/// <returns></returns>
 		Task<IEXResponse<IEnumerable<CurrencyConvertResponse>>> ConvertAsync(string symbols, string amount);
 
 		/// <summary>
 		/// <see cref="https://iexcloud.io/docs/api/#historical-daily"/>
 		/// </summary>
-		/// <value>700</value>
+		/// <param name="symbols"></param>
+		/// <param name="query"></param>
+		/// /// <param name="queryValue"></param>
 		/// <returns></returns>
 		Task<IEXResponse<IEnumerable<IEnumerable<CurrencyHistoricalRateResponse>>>> HistoricalDailyAsync(string symbols, string query, string queryValue);
 	}
