7e4e750a1935a508fe7b60133a49aee9b10e7e0f
Jamie Prentice <2858046+JamiePrentice@users.noreply.github.com>
2020-05-20 23:09:29 +0100

    Response caseing consistency

--- IEXSharp/Model/PremiumData/ExtractAlpha/Response/EsgCpscRecallsResponse.cs
+++ IEXSharp/Model/PremiumData/ExtractAlpha/Response/EsgCpscRecallsResponse.cs
@@ -2,21 +2,21 @@ namespace IEXSharp.Model.PremiumData.ExtractAlpha.Response
 {
 	public class EsgCpscRecallsResponse
 	{
-		public string eventId { get; set; }
-		public string masterName { get; set; }
-		public string name { get; set; }
-		public string recallID { get; set; }
-		public string recallNumber { get; set; }
-		public string description { get; set; }
+		public string EventId { get; set; }
+		public string MasterName { get; set; }
+		public string Name { get; set; }
+		public string RecallID { get; set; }
+		public string RecallNumber { get; set; }
+		public string Description { get; set; }
 		public string URL { get; set; }
-		public string title { get; set; }
-		public string consumerContact { get; set; }
-		public string lastPublishDate { get; set; }
-		public string updated { get; set; }
-		public string id { get; set; }
-		public string source { get; set; }
+		public string Title { get; set; }
+		public string ConsumerContact { get; set; }
+		public string LastPublishDate { get; set; }
+		public string Updated { get; set; }
+		public string Id { get; set; }
+		public string Source { get; set; }
 		public string key { get; set; }
-		public string subkey { get; set; }
-		public string date { get; set; }
+		public string Subkey { get; set; }
+		public string Date { get; set; }
 	}
 }
