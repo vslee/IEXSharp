1f7c13a97e9b7c6e662fc282411be66815e1a9df
Jamie Prentice <2858046+JamiePrentice@users.noreply.github.com>
2020-05-20 23:20:48 +0100

    Extract Alpha Tests

--- IEXSharpTest/Cloud/PremiumData/ExtractAlphaTest.cs
+++ IEXSharpTest/Cloud/PremiumData/ExtractAlphaTest.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using IEXSharp;
+using IEXSharp.Model;
+using IEXSharp.Model.PremiumData.ExtractAlpha.Response;
 using NUnit.Framework;
 
 namespace IEXSharpTest.Cloud.PremiumData
@@ -24,7 +27,151 @@ namespace IEXSharpTest.Cloud.PremiumData
 
 			Assert.IsNull(response.ErrorMessage);
 			Assert.IsNotNull(response.Data);
-			Assert.IsNotNull(response.Data.First().Key);
+			Assert.IsNotNull(response.Data.First().VolumeComponent);
+		}
+
+		[Test]
+		[TestCase("")]
+		[TestCase("AAPL")]
+		public async Task EsgCpscComplaintsAsyncTest(string symbol)
+		{
+			var response = await sandBoxClient.ExtractAlphaService.EsgCpscComplaintsAsync(symbol);
+
+			Assert.IsNull(response.ErrorMessage);
+			Assert.IsNotNull(response.Data);
+			Assert.IsNotNull(response.Data.First().ComplaintId);
+		}
+
+		[Test]
+		[TestCase("")]
+		[TestCase("AAPL")]
+		public async Task EsgCpscRecallsAsyncTest(string symbol)
+		{
+			var response = await sandBoxClient.ExtractAlphaService.EsgCpscRecallsAsync(symbol);
+
+			Assert.IsNull(response.ErrorMessage);
+			Assert.IsNotNull(response.Data);
+			Assert.IsNotNull(response.Data.First().RecallNumber);
+		}
+
+		[Test]
+		[TestCase("")]
+		[TestCase("AAPL")]
+		public async Task EsgDolVisaApplicationsAsyncTest(string symbol)
+		{
+			var response = await sandBoxClient.ExtractAlphaService.EsgDolVisaApplicationsAsync(symbol);
+
+			Assert.IsNull(response.ErrorMessage);
+			Assert.IsNotNull(response.Data);
+			Assert.IsNotNull(response.Data.First().TotalWorkers);
+		}
+
+		[Test]
+		[TestCase("")]
+		[TestCase("AAPL")]
+		public async Task EsgEpaEnforcementsAsyncTest(string symbol)
+		{
+			var response = await sandBoxClient.ExtractAlphaService.EsgEpaEnforcementsAsync(symbol);
+
+			Assert.IsNull(response.ErrorMessage);
+			Assert.IsNotNull(response.Data);
+			Assert.IsNotNull(response.Data.First().EnfSummaryText);
+		}
+
+		[Test]
+		[TestCase("")]
+		[TestCase("AAPL")]
+		public async Task EsgEpaMilestonesAsyncTest(string symbol)
+		{
+			var response = await sandBoxClient.ExtractAlphaService.EsgEpaMilestonesAsync(symbol);
+
+			Assert.IsNull(response.ErrorMessage);
+			Assert.IsNotNull(response.Data);
+			Assert.IsNotNull(response.Data.First().DefendantName);
+		}
+
+		[Test]
+		[TestCase("")]
+		[TestCase("AAPL")]
+		public async Task EsgFecIndividualCampaignContributionsAsyncTest(string symbol)
+		{
+			var response = await sandBoxClient.ExtractAlphaService.EsgFecIndividualCampaignContributionsAsync(symbol);
+
+			Assert.IsNull(response.ErrorMessage);
+			Assert.IsNotNull(response.Data);
+			Assert.IsNotNull(response.Data.First().TransactionAmt);
+		}
+
+		[Test]
+		[TestCase("")]
+		[TestCase("AAPL")]
+		public async Task EsgOshaInspectionsAsyncTest(string symbol)
+		{
+			var response = await sandBoxClient.ExtractAlphaService.EsgOshaInspectionsAsync(symbol);
+
+			Assert.IsNull(response.ErrorMessage);
+			Assert.IsNotNull(response.Data);
+			Assert.IsNotNull(response.Data.First().InspType);
+		}
+
+		[Test]
+		[TestCase("")]
+		[TestCase("AAPL")]
+		public async Task EsgSenateLobbyingAsyncTest(string symbol)
+		{
+			var response = await sandBoxClient.ExtractAlphaService.EsgSenateLobbyingAsync(symbol);
+
+			Assert.IsNull(response.ErrorMessage);
+			Assert.IsNotNull(response.Data);
+			Assert.IsNotNull(response.Data.First().ClientName);
+		}
+
+		[Test]
+		[TestCase("")]
+		[TestCase("AAPL")]
+		public async Task EsgUsaSpendingAsyncTest(string symbol)
+		{
+			var response = await sandBoxClient.ExtractAlphaService.EsgUsaSpendingAsync(symbol);
+
+			Assert.IsNull(response.ErrorMessage);
+			Assert.IsNotNull(response.Data);
+			Assert.IsNotNull(response.Data.First().VendorName);
+		}
+
+		[Test]
+		[TestCase("")]
+		[TestCase("AAPL")]
+		public async Task EsgUsptoPatentApplicationsAsyncTest(string symbol)
+		{
+			var response = await sandBoxClient.ExtractAlphaService.EsgUsptoPatentApplicationsAsync(symbol);
+
+			Assert.IsNull(response.ErrorMessage);
+			Assert.IsNotNull(response.Data);
+			Assert.IsNotNull(response.Data.First().FilingDate);
+		}
+
+		[Test]
+		[TestCase("")]
+		[TestCase("AAPL")]
+		public async Task EsgUsptoPatentGrantsAsyncTest(string symbol)
+		{
+			var response = await sandBoxClient.ExtractAlphaService.EsgUsptoPatentGrantsAsync(symbol);
+
+			Assert.IsNull(response.ErrorMessage);
+			Assert.IsNotNull(response.Data);
+			Assert.IsNotNull(response.Data.First().PatentNumber);
+		}
+
+		[Test]
+		[TestCase("")]
+		[TestCase("AAPL")]
+		public async Task TacticalModelOneAsyncTest(string symbol)
+		{
+			var response = await sandBoxClient.ExtractAlphaService.TacticalModelOneAsync(symbol);
+
+			Assert.IsNull(response.ErrorMessage);
+			Assert.IsNotNull(response.Data);
+			Assert.IsNotNull(response.Data.First().LiquidityShockComponent);
 		}
 	}
 }
