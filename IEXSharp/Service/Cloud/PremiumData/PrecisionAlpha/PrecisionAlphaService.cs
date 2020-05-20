79dc322427412c4ae3d63ac0d7771faaaffabc42
Jamie Prentice <2858046+JamiePrentice@users.noreply.github.com>
2020-05-20 23:32:52 +0100

    WIP PrecisionAlpha

---
+++ IEXSharp/Service/Cloud/PremiumData/PrecisionAlpha/PrecisionAlphaService.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using IEXSharp.Helper;
+using IEXSharp.Model;
+using IEXSharp.Model.PremiumData.PrecisionAlpha.Response;
+using IEXSharp.Service.Cloud.PremiumData.PrecisionAlpha;
+
+namespace IEXSharp.Service.Cloud.PremiumData.FraudFactors
+{
+	public class PrecisionAlphaService : IPrecisionAlphaService
+	{
+		private readonly ExecutorREST executor;
+
+		internal PrecisionAlphaService(ExecutorREST executor)
+		{
+			this.executor = executor;
+		}
+
+		public async Task<IEXResponse<IEnumerable<PriceDynamicsResponse>>> PriceDynamicsAsync(string symbol) =>
+			await executor.NoParamExecute<IEnumerable<PriceDynamicsResponse>>($"time-series/PREMIUM_PRECISION_ALPHA_PRICE_DYNAMICS/{symbol}");
+	}
+}
