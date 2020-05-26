ccdb2472e282b7fda0fbecd5ee0ec08f248bb14c
Jamie Prentice <2858046+JamiePrentice@users.noreply.github.com>
2020-05-26 23:30:36 +0100

    Endpoints for brain company

--- IEXSharp/Service/Cloud/PremiumData/BrainCompany/BrainCompanyService.cs
+++ IEXSharp/Service/Cloud/PremiumData/BrainCompany/BrainCompanyService.cs
@@ -3,7 +3,6 @@ using System.Threading.Tasks;
 using IEXSharp.Helper;
 using IEXSharp.Model;
 using IEXSharp.Model.PremiumData.FraudFactors.Response;
-using IEXSharp.Service.Cloud.PremiumData.FraudFactors;
 
 namespace IEXSharp.Service.Cloud.PremiumData.BrainCompany
 {
@@ -16,59 +15,37 @@ namespace IEXSharp.Service.Cloud.PremiumData.BrainCompany
 			this.executor = executor;
 		}
 
-		public Task<IEXResponse<IEnumerable<SimilarityIndexResponse>>> ThirtyDaySentimentIndicator(string symbol)
-		{
-			throw new System.NotImplementedException();
-		}
+		public async Task<IEXResponse<IEnumerable<SimilarityIndexResponse>>> ThirtyDaySentimentIndicator(string symbol) =>
+			await executor.NoParamExecute<IEnumerable<SimilarityIndexResponse>>($"time-series/PREMIUM_BRAIN_SENTIMENT_30_DAYS/{symbol}");
 
-		public Task<IEXResponse<IEnumerable<SimilarityIndexResponse>>> SevenDaySentimentIndicator(string symbol)
-		{
-			throw new System.NotImplementedException();
-		}
+		public async Task<IEXResponse<IEnumerable<SimilarityIndexResponse>>> SevenDaySentimentIndicator(string symbol) =>
+			await executor.NoParamExecute<IEnumerable<SimilarityIndexResponse>>($"time-series/PREMIUM_BRAIN_SENTIMENT_7_DAYS/{symbol}");
 
-		public Task<IEXResponse<IEnumerable<SimilarityIndexResponse>>> TwentyOneDayMachineLearningEstimatedReturnRanking(string symbol)
-		{
-			throw new System.NotImplementedException();
-		}
+		public async Task<IEXResponse<IEnumerable<SimilarityIndexResponse>>> TwentyOneDayMachineLearningEstimatedReturnRanking(string symbol) =>
+			await executor.NoParamExecute<IEnumerable<SimilarityIndexResponse>>($"time-series/PREMIUM_BRAIN_RANKING_21_DAYS/{symbol}");
 
-		public Task<IEXResponse<IEnumerable<SimilarityIndexResponse>>> TenDayMachineLearningEstimatedReturnRanking(string symbol)
-		{
-			throw new System.NotImplementedException();
-		}
+		public async Task<IEXResponse<IEnumerable<SimilarityIndexResponse>>> TenDayMachineLearningEstimatedReturnRanking(string symbol) =>
+			await executor.NoParamExecute<IEnumerable<SimilarityIndexResponse>>($"time-series/PREMIUM_BRAIN_RANKING_10_DAYS/{symbol}");
 
-		public Task<IEXResponse<IEnumerable<SimilarityIndexResponse>>> FiveDayMachineLearningEstimatedReturnRanking(string symbol)
-		{
-			throw new System.NotImplementedException();
-		}
+		public async Task<IEXResponse<IEnumerable<SimilarityIndexResponse>>> FiveDayMachineLearningEstimatedReturnRanking(string symbol) =>
+			await executor.NoParamExecute<IEnumerable<SimilarityIndexResponse>>($"time-series/PREMIUM_BRAIN_RANKING_5_DAYS/{symbol}");
 
-		public Task<IEXResponse<IEnumerable<SimilarityIndexResponse>>> ThreeDayMachineLearningEstimatedReturnRanking(string symbol)
-		{
-			throw new System.NotImplementedException();
-		}
+		public async Task<IEXResponse<IEnumerable<SimilarityIndexResponse>>> ThreeDayMachineLearningEstimatedReturnRanking(string symbol) =>
+			await executor.NoParamExecute<IEnumerable<SimilarityIndexResponse>>($"time-series/PREMIUM_BRAIN_RANKING_3_DAYS/{symbol}");
 
-		public Task<IEXResponse<IEnumerable<SimilarityIndexResponse>>> TwoDayMachineLearningEstimatedReturnRanking(string symbol)
-		{
-			throw new System.NotImplementedException();
-		}
+		public async Task<IEXResponse<IEnumerable<SimilarityIndexResponse>>> TwoDayMachineLearningEstimatedReturnRanking(string symbol) =>
+			await executor.NoParamExecute<IEnumerable<SimilarityIndexResponse>>($"time-series/PREMIUM_BRAIN_RANKING_2_DAYS/{symbol}");
 
-		public Task<IEXResponse<IEnumerable<SimilarityIndexResponse>>> LanguageMetricsOnCompanyFilingsQuarterlyAndAnnual(string symbol)
-		{
-			throw new System.NotImplementedException();
-		}
+		public async Task<IEXResponse<IEnumerable<SimilarityIndexResponse>>> LanguageMetricsOnCompanyFilingsQuarterlyAndAnnual(string symbol) =>
+			await executor.NoParamExecute<IEnumerable<SimilarityIndexResponse>>($"time-series/PREMIUM_BRAIN_LANGUAGE_METRICS_ALL/{symbol}");
 
-		public Task<IEXResponse<IEnumerable<SimilarityIndexResponse>>> LanguageMetricsOnCompanyFilingsAnnualOnly(string symbol)
-		{
-			throw new System.NotImplementedException();
-		}
+		public async Task<IEXResponse<IEnumerable<SimilarityIndexResponse>>> LanguageMetricsOnCompanyFilingsAnnualOnly(string symbol) =>
+			await executor.NoParamExecute<IEnumerable<SimilarityIndexResponse>>($"time-series/PREMIUM_BRAIN_LANGUAGE_METRICS_10K/{symbol}");
 
-		public Task<IEXResponse<IEnumerable<SimilarityIndexResponse>>> DifferencesInLanguageMetricsOnCompanyFilingsQuarterlyAndAnnualFromPriorPeriod(string symbol)
-		{
-			throw new System.NotImplementedException();
-		}
+		public async Task<IEXResponse<IEnumerable<SimilarityIndexResponse>>> DifferencesInLanguageMetricsOnCompanyFilingsQuarterlyAndAnnualFromPriorPeriod(string symbol) =>
+			await executor.NoParamExecute<IEnumerable<SimilarityIndexResponse>>($"time-series/PREMIUM_BRAIN_LANGUAGE_DIFFERENCES_ALL/{symbol}");
 
-		public Task<IEXResponse<IEnumerable<SimilarityIndexResponse>>> DifferencesInLanguageMetricsOnCompanyAnnualFilingsFromPriorYear(string symbol)
-		{
-			throw new System.NotImplementedException();
-		}
+		public async Task<IEXResponse<IEnumerable<SimilarityIndexResponse>>> DifferencesInLanguageMetricsOnCompanyAnnualFilingsFromPriorYear(string symbol) =>
+			await executor.NoParamExecute<IEnumerable<SimilarityIndexResponse>>($"time-series/PREMIUM_BRAIN_LANGUAGE_DIFFERENCES_10K/{symbol}");
 	}
 }
