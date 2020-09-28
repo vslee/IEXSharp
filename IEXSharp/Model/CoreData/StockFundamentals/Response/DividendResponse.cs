71597cb672b54adde0430bb4c8566a94f9f23b30
Amine Mejaouel <45773049+amine-mejaouel@users.noreply.github.com>
2020-09-28 22:15:53 +0200

    Remove DividendAsync from StockService
    Remove DividendAsync from StockService since the same method
    is already implemented in StockFundamentalsService.DividendsBasicAsync
    
    The removal of this method is probably been missed in
    https://github.com/vslee/IEXSharp/commit/ca5029eb37afe7ac94464bd3d903fd35a63c6f9b

--- IEXSharp/Model/CoreData/StockFundamentals/Response/DividendResponse.cs
+++ IEXSharp/Model/CoreData/StockFundamentals/Response/DividendResponse.cs
@@ -3,8 +3,6 @@ using IEXSharp.Model.Shared.Response;
 
 namespace IEXSharp.Model.CoreData.StockFundamentals.Response
 {
-	public class DividendV1Response : DividendV1 { }
-
 	public class DividendBasicResponse : Dividend
 	{
 		public DateTime date { get; set; }
--- IEXSharp/Model/Shared/Response/Dividend.cs
+++ IEXSharp/Model/Shared/Response/Dividend.cs
@@ -12,19 +12,6 @@ namespace IEXSharp.Model.Shared.Response
 		quarterly = 2,
 	}
 
-	public class DividendV1
-	{
-		public DateTime exDate { get; set; }
-		public DateTime paymentDate { get; set; }
-		public DateTime recordDate { get; set; }
-		public DateTime declaredDate { get; set; }
-		public decimal amount { get; set; }
-		public string flag { get; set; }
-		public string type { get; set; }
-		public string qualified { get; set; }
-		public decimal indicated { get; set; }
-	}
-
 	public class Dividend
 	{
 		public DateTime exDate { get; set; }
