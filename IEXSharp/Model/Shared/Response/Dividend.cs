caee53a56203e2fcc5f0aaa73603bea8f441abcf
Amine Mejaouel <45773049+amine-mejaouel@users.noreply.github.com>
2020-09-29 02:46:33 +0200

    Fix dividend amount Nullable amount response (#67)

--- IEXSharp/Model/Shared/Response/Dividend.cs
+++ IEXSharp/Model/Shared/Response/Dividend.cs
@@ -18,7 +18,7 @@ namespace IEXSharp.Model.Shared.Response
 		public DateTime? paymentDate { get; set; }
 		public DateTime recordDate { get; set; }
 		public DateTime? declaredDate { get; set; }
-		public decimal amount { get; set; }
+		public decimal? amount { get; set; }
 		public string flag { get; set; }
 		public DividendFlag DividendFlag =>
 			(DividendFlag)Enum.Parse(typeof(DividendFlag), flag);
