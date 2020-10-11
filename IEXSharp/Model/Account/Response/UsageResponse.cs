615fc646f2f7432c0e9172dfd4af76c187192338
Jamie Prentice <2858046+JamiePrentice@users.noreply.github.com>
2020-10-11 11:56:05 +0100

    Updating usage response format

--- IEXSharp/Model/Account/Response/UsageResponse.cs
+++ IEXSharp/Model/Account/Response/UsageResponse.cs
@@ -13,17 +13,8 @@ namespace IEXSharp.Model.Account.Response
 	{
 		public long monthlyUsage { get; set; }
 		public long monthlyPayAsYouGo { get; set; }
-		public Dictionary<DateTime, long> dailyUsage { get; set; }
-		public Dictionary<string, long> tokenUsage { get; set; }
-		public UsageResponseKeyUsage keyUsage { get; set; }
-	}
-
-	public class UsageResponseKeyUsage
-	{
-		public long IEX_TOPS { get; set; }
-		public int COMPANY { get; set; }
-		public long IEX_STATS { get; set; }
-		public long EARNINGS { get; set; }
-		public long STOCK_QUOTES { get; set; }
+		public Dictionary<DateTime, string> dailyUsage { get; set; }
+		public Dictionary<string, string> tokenUsage { get; set; }
+		public Dictionary<string, string> keyUsage { get; set; }
 	}
 }
