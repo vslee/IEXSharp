d4e913dbad1e1af0f19995e6c5ed516ca46e9cd3
Jamie Prentice <2858046+JamiePrentice@users.noreply.github.com>
2020-12-12 20:46:42 +0000

    Adding missing fields for Earning.cs

--- IEXSharp/Model/Shared/Response/Earning.cs
+++ IEXSharp/Model/Shared/Response/Earning.cs
@@ -4,16 +4,24 @@ namespace IEXSharp.Model.Shared.Response
 {
 	public class Earning
 	{
-		public decimal actualEPS { get; set; }
-		public decimal consensusEPS { get; set; }
-		public string announceTime { get; set; }
-		public int numberOfEstimates { get; set; }
-		public decimal EPSSurpriseDollar { get; set; }
 		public DateTime EPSReportDate { get; set; }
-		public string fiscalPeriod { get; set; }
-		public DateTime fiscalEndDate { get; set; }
-		public decimal yearAgo { get; set; }
-		public decimal yearAgoChangePercent { get; set; }
+		public double EPSSurpriseDollar { get; set; }
+		public double EPSSurpriseDollarPercent { get; set; }
+		public double actualEPS { get; set; }
+		public string announceTime { get; set; }
+		public double consensusEPS { get; set; }
 		public string currency { get; set; }
+		public DateTime fiscalEndDate { get; set; }
+		public string fiscalPeriod { get; set; }
+		public int numberOfEstimates { get; set; }
+		public string periodType { get; set; }
+		public string symbol { get; set; }
+		public double yearAgo { get; set; }
+		public double yearAgoChangePercent { get; set; }
+		public string id { get; set; }
+		public string key { get; set; }
+		public string subkey { get; set; }
+		public long date { get; set; }
+		public long updated { get; set; }
 	}
 }
