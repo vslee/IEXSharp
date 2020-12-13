0199c0184a3fe173134cdf467113d71757e39f4f
Jamie Prentice <2858046+JamiePrentice@users.noreply.github.com>
2020-12-13 13:26:24 +0000

    HistoricalPriceResponse Update

--- IEXSharp/Model/CoreData/StockPrices/Response/HistoricalPriceResponse.cs
+++ IEXSharp/Model/CoreData/StockPrices/Response/HistoricalPriceResponse.cs
@@ -9,21 +9,32 @@ namespace IEXSharp.Model.CoreData.StockPrices.Response
 		/// <summary>
 		/// Use DateTimeExtensions.GetTimestampInUTC(), which takes into account both 'date' and 'minute' and timezone
 		/// </summary>
-		public string date { get; set; }
 		public string minute { get; set; }
-		public decimal? open { get; set; }
 		public decimal? close { get; set; }
 		public decimal? high { get; set; }
 		public decimal? low { get; set; }
+		public decimal? open { get; set; }
+		public string symbol { get; set; }
 		public long? volume { get; set; }
+		public string id { get; set; }
+		public string key { get; set; }
+		public string subkey { get; set; }
+		public string date { get; set; }
+		public long? updated { get; set; }
+		public decimal? changeOverTime { get; set; }
+		public decimal? marketChangeOverTime { get; set; }
 		public decimal? uOpen { get; set; }
 		public decimal? uClose { get; set; }
 		public decimal? uHigh { get; set; }
 		public decimal? uLow { get; set; }
 		public long? uVolume { get; set; }
+		public decimal? fOpen { get; set; }
+		public decimal? fClose { get; set; }
+		public decimal? fHigh { get; set; }
+		public decimal? fLow { get; set; }
+		public long? fVolume { get; set; }
+		public string label { get; set; }
 		public decimal? change { get; set; }
 		public decimal? changePercent { get; set; }
-		public decimal? changeOverTime { get; set; }
-		public string symbol { get; set; }
 	}
 }
