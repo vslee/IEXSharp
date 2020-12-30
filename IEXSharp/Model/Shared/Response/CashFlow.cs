49b98743b134fa21c079719082181259373e1959
Amine Mejaouel <45773049+amine-mejaouel@users.noreply.github.com>
2020-12-30 11:52:40 +0100

    update the Cashflow fields

--- IEXSharp/Model/Shared/Response/CashFlow.cs
+++ IEXSharp/Model/Shared/Response/CashFlow.cs
@@ -2,25 +2,33 @@
 
 namespace IEXSharp.Model.Shared.Response
 {
-	public class Cashflow
-	{
-		public DateTime reportDate { get; set; }
-		public DateTime fiscalDate { get; set; }
-		public string currency { get; set; }
-		public long netIncome { get; set; }
-		public long depreciation { get; set; }
-		public long? changesInReceivables { get; set; }
-		public long changesInInventories { get; set; }
-		public long cashChange { get; set; }
-		public long cashFlow { get; set; }
-		public long capitalExpenditures { get; set; }
-		public long investments { get; set; }
-		public long investingActivityOther { get; set; }
-		public long totalInvestingCashFlows { get; set; }
-		public long dividendsPaid { get; set; }
-		public long netBorrowings { get; set; }
-		public long otherFinancingCashFlows { get; set; }
-		public long cashFlowFinancing { get; set; }
-		public long? exchangeRateEffect { get; set; }
-	}
+    public class Cashflow
+    {
+        public DateTime reportDate { get; set; }
+        public DateTime fiscalDate { get; set; }
+        public string currency { get; set; }
+        public decimal? netIncome { get; set; }
+        public decimal? depreciation { get; set; }
+        public decimal? changesInReceivables { get; set; }
+        public decimal? changesInInventories { get; set; }
+        public decimal? cashChange { get; set; }
+        public decimal? cashFlow { get; set; }
+        public decimal? capitalExpenditures { get; set; }
+        public decimal? investments { get; set; }
+        public decimal? investingActivityOther { get; set; }
+        public decimal? totalInvestingCashFlows { get; set; }
+        public decimal? dividendsPaid { get; set; }
+        public decimal? netBorrowings { get; set; }
+        public decimal? otherFinancingCashFlows { get; set; }
+        public decimal? cashFlowFinancing { get; set; }
+        public decimal? exchangeRateEffect { get; set; }
+        public string id { get; set; }
+        public string key { get; set; }
+        public string subkey { get; set; }
+        public string symbol { get; set; }
+        public string filingType { get; set; }
+        public int fiscalQuarter { get; set; }
+        public int fiscalYear { get; set; }
+        public long updated { get; set; }
+    }
 }
