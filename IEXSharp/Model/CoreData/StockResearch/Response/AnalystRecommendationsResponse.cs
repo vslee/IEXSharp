19f9cf7d2faa6966269c70cb8284f197b27a24d3
doclove <jlove@cyberlancers.com>
2021-03-11 09:26:48 -0500

    Fixed an issue where analyst recommendations were returning null

--- IEXSharp/Model/CoreData/StockResearch/Response/AnalystRecommendationsResponse.cs
+++ IEXSharp/Model/CoreData/StockResearch/Response/AnalystRecommendationsResponse.cs
@@ -5,12 +5,12 @@ namespace IEXSharp.Model.CoreData.StockResearch.Response
 		public long? consensusEndDate { get; set; }
 		public long consensusStartDate { get; set; }
 		public long? corporateActionsAppliedDate { get; set; }
-		public int ratingBuy { get; set; }
-		public int ratingOverweight { get; set; }
-		public int ratingHold { get; set; }
-		public int ratingUnderweight { get; set; }
-		public int ratingSell { get; set; }
-		public int ratingNone { get; set; }
+		public int? ratingBuy { get; set; }
+		public int? ratingOverweight { get; set; }
+		public int? ratingHold { get; set; }
+		public int? ratingUnderweight { get; set; }
+		public int? ratingSell { get; set; }
+		public int? ratingNone { get; set; }
 		public decimal ratingScaleMark { get; set; }
 	}
 }
